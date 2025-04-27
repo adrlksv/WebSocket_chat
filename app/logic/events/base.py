d1f9bdfdbee1755760a2368374519537f1391977
Andrey <Qwerty123Andrey@bk.ru>
2025-04-27 23:22:39 +0300

    mediator

---
+++ app/logic/events/base.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Generic, TypeVar, Any
+
+from domain.events.base import BaseEvent
+
+
+ET = TypeVar(name='ET', bound=BaseEvent)
+ER = TypeVar(name='ER', bound=Any)
+
+@dataclass
+class EventHandler(ABC, Generic[ET]):
+    @abstractmethod
+    def handle(self, event: ET) -> ER:
+        ...
