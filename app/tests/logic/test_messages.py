926bd03735e81b5aaefbc3d882630762f1fcc1e3
Andrey <Qwerty123Andrey@bk.ru>
2025-04-30 20:23:32 +0300

    repositories and mediator fix

--- app/tests/logic/test_messages.py
+++ app/tests/logic/test_messages.py
@@ -1,16 +1,37 @@
 import pytest
 
 from domain.entities.messages import Chat
+from domain.values.messages import Title
 from infra.repositories.messages import BaseChatRepository
 from logic.commands.messages import CreateChatCommand
+from logic.exceptions.messages import ChatWithThatTitleAlreadyExistsException
 from logic.mediator import Mediator
+
+from faker import Faker
 
 
 @pytest.mark.asyncio
 async def test_create_chat_command_success(
         chat_repository: BaseChatRepository,
         mediator: Mediator,
+        faker: Faker
 ):
-    chat: Chat = (await mediator.handle_command(CreateChatCommand(title='gigaTitle')))[0]
+    chat: Chat = (await mediator.handle_command(CreateChatCommand(title=faker.text())))[0]
 
-    assert chat_repository.check_chat_exists_by_title(title=chat.title.as_generic_type())
+    assert await chat_repository.check_chat_exists_by_title(title=chat.title.as_generic_type())
+
+
+@pytest.mark.asyncio
+async def test_create_chat_command_title_already_exists(
+        chat_repository: BaseChatRepository,
+        mediator: Mediator,
+        faker: Faker
+):
+    title_text = faker.text()
+    chat = Chat(title=Title(title_text))
+    await chat_repository.add_chat(chat)
+
+    with pytest.raises(ChatWithThatTitleAlreadyExistsException):
+        await mediator.handle_command(CreateChatCommand(title=title_text))[0]
+
+    assert len(chat_repository._saved_chats) == 1
